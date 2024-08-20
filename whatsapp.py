3be8ae68b665d97b2a98be1036689a4314e8d3c3
Giorgi Makharoblidze <81810382+Bluxuna@users.noreply.github.com>
2024-08-20 12:08:27 +0400

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,14 +1,14 @@
-from twilio.rest import Client
+# from twilio.rest import Client
 
-def send_reservation(info: str):
+# def send_reservation(info: str):
     
-    account_sid = 'AC78b6305c73bc1aa55bbfc599ee2cf982'
-    auth_token = 'a97074a015fe7577345930434552d5f1'
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-        from_='whatsapp:+14155238886',
-        body=f'{info}',
-        to='whatsapp:+995598311309'
-    )
+#     account_sid = 'AC78b6305c73bc1aa55bbfc599ee2cf982'
+#     auth_token = 'a97074a015fe7577345930434552d5f1'
+#     client = Client(account_sid, auth_token)
+#     message = client.messages.create(
+#         from_='whatsapp:+14155238886',
+#         body=f'{info}',
+#         to='whatsapp:+995598311309'
+#     )
 
-    print(message.sid)
+#     print(message.sid)
